eddbf638e222e2043ff642414d500f2e78a00a63
quarties <m.sypko@text.com>
2025-02-02 18:23:33 +0100

    deputy

--- src/_roles/index.ts
+++ src/_roles/index.ts
@@ -4,5 +4,6 @@ import {Arsonist} from "@/_roles/arsonist";
 import {Aurial} from "@/_roles/aurial";
 import {Blackmailer} from "@/_roles/blackmailer";
 import {Bomber} from "@/_roles/bomber";
+import {Deputy} from "@/_roles/deputy";
 
-export const Roles = [Altruist, Amnesiac, Arsonist, Aurial, Blackmailer, Bomber].sort();
+export const Roles = [Altruist, Amnesiac, Arsonist, Aurial, Blackmailer, Bomber, Deputy].sort();
