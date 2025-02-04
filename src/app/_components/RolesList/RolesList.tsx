8f433c6890524679222afc57dd8e9aada9ba6de8
quarties <m.sypko@text.com>
2025-02-04 02:00:36 +0100

    working filters logic

--- src/app/_components/RolesList/RolesList.tsx
+++ src/app/_components/RolesList/RolesList.tsx
@@ -1,39 +1,101 @@
 'use client';
 
 import debounce from "lodash.debounce";
-import {ChangeEvent, createContext, useMemo, useState} from "react";
+import {createContext, useCallback, useMemo, useState} from "react";
 import {Roles} from "@/roles";
 import {Modifiers} from "@/modifiers";
 import {Search} from "@/app/_components";
 import {RoleCard} from "./RoleCard/RoleCard";
+import {RoleOrModifierTypes} from "@/constants/rolesAndModifiers";
+import {Teams} from "@/constants/teams";
 
 const RolesAndModifiers = [...Roles, ...Modifiers];
 
+export enum RoleFilters {
+    Role = 'Role',
+    Modifier = 'Modifier',
+}
+
+export enum TeamFilters {
+    Crewmate = 'Crewmate',
+    Neutral = 'Neutral',
+    Impostor = 'Impostor',
+}
+
 type RolesListContextType = {
     searchValue: string;
-    search: (e: ChangeEvent<HTMLInputElement>) => void;
+    search: (x: string) => void;
+    filter: (x: RoleFilters|TeamFilters) => void;
 };
 
 export const RolesListContext = createContext<RolesListContextType>({
     searchValue: '',
     search: () => {
-    }
+    },
+    filter: () => {
+    },
 });
 
 export const RolesList = () => {
     const [searchValue, setSearchValue] = useState<string>('');
+    const [typeFilterValue, setTypeFilterValue] = useState<RoleFilters|null>(null);
+    const [teamFilterValue, setTeamFilterValue] = useState<TeamFilters|null>(null);
 
-    const search = useMemo(() => debounce((e: ChangeEvent<HTMLInputElement>) => {
-        setSearchValue(e.target.value);
-    }, 300), []);
+    const search = useMemo(() => debounce(setSearchValue, 300), []);
+
+    const filter = useCallback((filterValue: RoleFilters|TeamFilters) => {
+        if (filterValue in RoleFilters) {
+            setTypeFilterValue(filterValue as RoleFilters);
+        }
+
+        if (filterValue in TeamFilters) {
+            setTeamFilterValue(filterValue as TeamFilters);
+        }
+    }, []);
 
     const results = useMemo(
-        () => RolesAndModifiers.filter(({name}) => name.toLowerCase().startsWith(searchValue.toLowerCase())),
-        [searchValue]
+        () => {
+            let rolesAndModifiers = RolesAndModifiers;
+
+            if (searchValue) {
+                rolesAndModifiers = rolesAndModifiers.filter(
+                    ({name}) =>
+                        name.toLowerCase().includes(searchValue.toLowerCase())
+                )
+            }
+
+            switch (typeFilterValue) {
+                case RoleFilters.Role:
+                    rolesAndModifiers = rolesAndModifiers.filter(({type}) => type === RoleOrModifierTypes.Role);
+                    break;
+                case RoleFilters.Modifier:
+                    rolesAndModifiers = rolesAndModifiers.filter(({type}) => type === RoleOrModifierTypes.Modifier);
+                    break;
+                default:
+                    break;
+            }
+
+            switch (teamFilterValue) {
+                case TeamFilters.Crewmate:
+                    rolesAndModifiers = rolesAndModifiers.filter(({team}) => team === Teams.Crewmate || team === Teams.All);
+                    break;
+                case TeamFilters.Neutral:
+                    rolesAndModifiers = rolesAndModifiers.filter(({team}) => team === Teams.Neutral || team === Teams.All);
+                    break;
+                case TeamFilters.Impostor:
+                    rolesAndModifiers = rolesAndModifiers.filter(({team}) => team === Teams.Neutral || team === Teams.All);
+                    break;
+                default:
+                    break;
+            }
+
+            return rolesAndModifiers;
+        },
+        [searchValue, typeFilterValue, teamFilterValue]
     );
 
     return (
-        <RolesListContext.Provider value={{searchValue, search}}>
+        <RolesListContext.Provider value={{searchValue, search, filter}}>
             <Search/>
             <main>
                 <div className="grid grid-cols-1 gap-y-5">
--- src/app/_components/Search.tsx
+++ src/app/_components/Search.tsx
@@ -5,7 +5,11 @@ export const Search = () => {
     const {search} = useContext(RolesListContext);
 
     return (
-        <input type="search" placeholder="Wyszukaj rolę..."
-               className="w-full p-3 text-md bg-zinc-900/75 border-2 rounded-md border-search" onChange={search}/>
+        <input
+            type="search"
+            placeholder="Wyszukaj rolę..."
+            className="w-full p-3 text-md bg-zinc-900/75 border-2 rounded-md border-search"
+            onChange={(e) => search(e.target.value)}
+        />
     );
 }
