b0e5776fccea4be4a8970c0a758baaa571807c21
quarties <m.sypko@text.com>
2025-02-11 18:52:42 +0100

    RolesList as reusable component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import {RolesList} from "./_components";
+import {Roles} from "@/roles";
+import {Modifiers} from "@/modifiers";
 
 export default function Home() {
     return (
         <div className="grid grid-cols-1 gap-y-5">
-            <RolesList/>
+            <RolesList roles={Roles} modifiers={Modifiers}/>
         </div>
     );
 }
