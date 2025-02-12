79d232f7e3298fa8649a645f762423db7b55e8cb
quarties <m.sypko@text.com>
2025-02-12 02:20:57 +0100

    fix mobile

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Custom404() {
     return (
         <div className="flex flex-col items-center justify-center">
-            <div className="inline-block text-center mt-20 mx-auto">
+            <div className="inline-block text-center mt-0 sm:mt-20 mx-auto">
                 <Image src="/images/404.png" alt="Detective" width={200} height={200}
                        className="inline-block scale-125 drop-shadow-2xl"/>
                 <h3 className="font-brook text-6xl drop-shadow-md">Nic tu nie ma</h3>
