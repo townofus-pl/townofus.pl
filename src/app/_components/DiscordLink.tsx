73f2bd58d3264cfaf12326ddf2876852b5f7723e
quarties <m.sypko@text.com>
2025-02-12 02:29:15 +0100

    clickable header title

--- src/app/_components/DiscordLink.tsx
+++ src/app/_components/DiscordLink.tsx
@@ -2,10 +2,10 @@ import Link from "next/link";
 import Image from "next/image";
 
 export const DiscordLink = () => (
-    <div
-        className="fixed top-4 right-4 z-10 w-14 h-14 px-2 py-3 bg-discord-blurple text-white text-center rounded-lg drop-shadow-md hover:scale-110 hover:drop-shadow-lg transition-transform duration-300">
-        <Link href="https://discord.townofus.pl" target="_blank" rel="noreferrer" title="Dołącz do naszego Discorda!">
+    <Link href="https://discord.townofus.pl" target="_blank" rel="noreferrer" title="Dołącz do naszego Discorda!">
+        <div
+            className="fixed top-4 right-4 z-10 w-14 h-14 px-2 py-3 bg-discord-blurple text-white text-center rounded-lg drop-shadow-md hover:scale-110 hover:drop-shadow-lg transition-transform duration-300">
             <Image src="/images/discord-logo.svg" alt="Discord" width={127} height={96}/>
-        </Link>
-    </div>
+        </div>
+    </Link>
 );
--- src/app/_components/Header.tsx
+++ src/app/_components/Header.tsx
@@ -1,6 +1,10 @@
+import Link from "next/link";
+
 export const Header = () => (
     <header className="grid grid-cols-1 gap-6 bg-zinc-900/50 px-5 py-10 rounded-xl mb-5">
-        <h1 className="text-7xl font-bold font-brook">Town Of Us - Role</h1>
+        <h1 className="text-7xl font-bold font-brook">
+            <Link href="/">Town Of Us - Role</Link>
+        </h1>
         <h2 className="text-3xl font-brook">
             Lista ról i modyfikatorów dostępnych w Town Of Us v5.2.1
             <span className="md:ml-2.5 font-barlow text-base text-gray-500 block md:inline">by Malkiz, Quarties & OrzechMC</span>
