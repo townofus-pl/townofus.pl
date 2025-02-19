f05e5638a50897c0656f03ddd7ac283d587fdf2a
MalkizH3 <114110029+MalkizH3@users.noreply.github.com>
2025-02-19 18:25:00 +0100

    Update doomsayer.ts

--- src/roles/doomsayer.ts
+++ src/roles/doomsayer.ts
@@ -1,7 +1,6 @@
 import {Role, RoleOrModifierTypes} from "@/constants/rolesAndModifiers";
 import {Teams} from "@/constants/teams";
 import {probabilityOfAppearing} from "@/constants/settings";
-import {CommonAbilities} from "@/constants/abilities";
 
 export const DoomsayerAbilities = {
     Observe: {
