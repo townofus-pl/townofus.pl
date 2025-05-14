8393d127bca921a92a8ab97b1a34a9f58bbec871
Michał Cichocki <41922167+Klaym3n@users.noreply.github.com>
2025-05-14 18:06:43 +0200

    Update doomsayer.ts (typo)

--- src/roles/doomsayer.ts
+++ src/roles/doomsayer.ts
@@ -23,7 +23,7 @@ export const Doomsayer: Role = {
             value: 10,
             type: SettingTypes.Time,
         },
-        "Dooomsayer Guesses All Roles At Once": {
+        "Doomsayer Guesses All Roles At Once": {
             value: true,
             type: SettingTypes.Boolean,
         },
