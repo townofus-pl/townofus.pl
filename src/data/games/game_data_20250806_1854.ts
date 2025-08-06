3518bf6313f2364d81d2db9922d742c25668abaa
MalkizH3 <pigipiotr@gmail.com>
2025-08-06 19:07:11 +0200

    gra 4

---
+++ src/data/games/game_data_20250806_1854.ts
@@ -0,0 +1,675 @@
+// Generated game data - 2025-08-06 19:05:16
+
+export interface GameMetadata {
+  startTime: string;
+  endTime: string;
+  duration: string;
+  playerCount: number;
+  map: string;
+  maxTasks?: number;
+}
+
+export interface PlayerStats {
+  playerName: string;
+  roleHistory: string[];
+  modifiers: string[];
+  win: number;
+  disconnected: number;
+  initialRolePoints: number;
+  correctKills: number;
+  incorrectKills: number;
+  correctProsecutes: number;
+  incorrectProsecutes: number;
+  correctGuesses: number;
+  incorrectGuesses: number;
+  correctDeputyShoots: number;
+  incorrectDeputyShoots: number;
+  correctJailorExecutes: number;
+  incorrectJailorExecutes: number;
+  correctMedicShields: number;
+  incorrectMedicShields: number;
+  correctWardenFortifies: number;
+  incorrectWardenFortifies: number;
+  janitorCleans: number;
+  completedTasks: number;
+  survivedRounds: number;
+  correctAltruistRevives: number;
+  incorrectAltruistRevives: number;
+  correctSwaps: number;
+  incorrectSwaps: number;
+  totalPoints: number;
+}
+
+export interface GameEvent {
+  timestamp: string;
+  description: string;
+}
+
+export interface MeetingData {
+  meetingNumber: number;
+  deathsSinceLastMeeting: string[];
+  votes: { [playerId: string]: string[] };
+  skipVotes: string[];
+  noVotes: string[];
+  blackmailedPlayers: string[];
+  jailedPlayers: string[];
+  wasTie: boolean;
+  wasBlessed: boolean;
+}
+
+export interface GameData {
+  metadata: GameMetadata;
+  players: PlayerStats[];
+  gameEvents: GameEvent[];
+  meetings: MeetingData[];
+}
+
+export const gameData: GameData = {
+  metadata: {
+    startTime: "2025-08-06 18:54:39",
+    endTime: "2025-08-06 19:05:16",
+    duration: "10:37",
+    playerCount: 15,
+    map: "Polus",
+    maxTasks: 9,
+  },
+  players: [
+    {
+      playerName: "ziomson",
+      roleHistory: ["Trapper"],
+      modifiers: [],
+      win: 0,
+      disconnected: 0,
+      initialRolePoints: 2,
+      correctKills: 0,
+      incorrectKills: 0,
+      correctProsecutes: 0,
+      incorrectProsecutes: 0,
+      correctGuesses: 0,
+      incorrectGuesses: 0,
+      correctDeputyShoots: 0,
+      incorrectDeputyShoots: 0,
+      correctJailorExecutes: 0,
+      incorrectJailorExecutes: 0,
+      correctMedicShields: 0,
+      incorrectMedicShields: 0,
+      correctWardenFortifies: 0,
+      incorrectWardenFortifies: 0,
+      janitorCleans: 0,
+      completedTasks: 9,
+      survivedRounds: 2,
+      correctAltruistRevives: 0,
+      incorrectAltruistRevives: 0,
+      correctSwaps: 0,
+      incorrectSwaps: 0,
+      totalPoints: 3.3,
+    },
+    {
+      playerName: "brubel",
+      roleHistory: ["Warlock"],
+      modifiers: ["Saboteur"],
+      win: 1,
+      disconnected: 0,
+      initialRolePoints: 2,
+      correctKills: 1,
+      incorrectKills: 0,
+      correctProsecutes: 0,
+      incorrectProsecutes: 0,
+      correctGuesses: 3,
+      incorrectGuesses: 0,
+      correctDeputyShoots: 0,
+      incorrectDeputyShoots: 0,
+      correctJailorExecutes: 0,
+      incorrectJailorExecutes: 0,
+      correctMedicShields: 0,
+      incorrectMedicShields: 0,
+      correctWardenFortifies: 0,
+      incorrectWardenFortifies: 0,
+      janitorCleans: 0,
+      completedTasks: 0,
+      survivedRounds: 3,
+      correctAltruistRevives: 0,
+      incorrectAltruistRevives: 0,
+      correctSwaps: 0,
+      incorrectSwaps: 0,
+      totalPoints: 11.6,
+    },
+    {
+      playerName: "MikoTheOwl",
+      roleHistory: ["Hunter"],
+      modifiers: ["Giant"],
+      win: 0,
+      disconnected: 0,
+      initialRolePoints: 2,
+      correctKills: 0,
+      incorrectKills: 0,
+      correctProsecutes: 0,
+      incorrectProsecutes: 0,
+      correctGuesses: 0,
+      incorrectGuesses: 0,
+      correctDeputyShoots: 0,
+      incorrectDeputyShoots: 0,
+      correctJailorExecutes: 0,
+      incorrectJailorExecutes: 0,
+      correctMedicShields: 0,
+      incorrectMedicShields: 0,
+      correctWardenFortifies: 0,
+      incorrectWardenFortifies: 0,
+      janitorCleans: 0,
+      completedTasks: 4,
+      survivedRounds: 3,
+      correctAltruistRevives: 0,
+      incorrectAltruistRevives: 0,
+      correctSwaps: 0,
+      incorrectSwaps: 0,
+      totalPoints: 3.0,
+    },
+    {
+      playerName: "Cleopatrie",
+      roleHistory: ["Janitor"],
+      modifiers: ["Disperser"],
+      win: 1,
+      disconnected: 0,
+      initialRolePoints: 2,
+      correctKills: 1,
+      incorrectKills: 0,
+      correctProsecutes: 0,
+      incorrectProsecutes: 0,
+      correctGuesses: 0,
+      incorrectGuesses: 0,
+      correctDeputyShoots: 0,
+      incorrectDeputyShoots: 0,
+      correctJailorExecutes: 0,
+      incorrectJailorExecutes: 0,
+      correctMedicShields: 0,
+      incorrectMedicShields: 0,
+      correctWardenFortifies: 0,
+      incorrectWardenFortifies: 0,
+      janitorCleans: 0,
+      completedTasks: 0,
+      survivedRounds: 1,
+      correctAltruistRevives: 0,
+      incorrectAltruistRevives: 0,
+      correctSwaps: 0,
+      incorrectSwaps: 0,
+      totalPoints: 8.2,
+    },
+    {
+      playerName: "Mamika",
+      roleHistory: ["Executioner"],
+      modifiers: ["Sleuth"],
+      win: 0,
+      disconnected: 0,
+      initialRolePoints: 3,
+      correctKills: 0,
+      incorrectKills: 0,
+      correctProsecutes: 0,
+      incorrectProsecutes: 0,
+      correctGuesses: 0,
+      incorrectGuesses: 0,
+      correctDeputyShoots: 0,
+      incorrectDeputyShoots: 0,
+      correctJailorExecutes: 0,
+      incorrectJailorExecutes: 0,
+      correctMedicShields: 0,
+      incorrectMedicShields: 0,
+      correctWardenFortifies: 0,
+      incorrectWardenFortifies: 0,
+      janitorCleans: 0,
+      completedTasks: 0,
+      survivedRounds: 2,
+      correctAltruistRevives: 0,
+      incorrectAltruistRevives: 0,
+      correctSwaps: 0,
+      incorrectSwaps: 0,
+      totalPoints: 3.4,
+    },
+    {
+      playerName: "Bushej",
+      roleHistory: ["Swapper"],
+      modifiers: [],
+      win: 0,
+      disconnected: 0,
+      initialRolePoints: 2,
+      correctKills: 0,
+      incorrectKills: 0,
+      correctProsecutes: 0,
+      incorrectProsecutes: 0,
+      correctGuesses: 0,
+      incorrectGuesses: 0,
+      correctDeputyShoots: 0,
+      incorrectDeputyShoots: 0,
+      correctJailorExecutes: 0,
+      incorrectJailorExecutes: 0,
+      correctMedicShields: 0,
+      incorrectMedicShields: 0,
+      correctWardenFortifies: 0,
+      incorrectWardenFortifies: 0,
+      janitorCleans: 0,
+      completedTasks: 6,
+      survivedRounds: 3,
+      correctAltruistRevives: 0,
+      incorrectAltruistRevives: 0,
+      correctSwaps: 0,
+      incorrectSwaps: 0,
+      totalPoints: 3.2,
+    },
+    {
+      playerName: "Dusia",
+      roleHistory: ["Lookout"],
+      modifiers: [],
+      win: 0,
+      disconnected: 0,
+      initialRolePoints: 2,
+      correctKills: 0,
+      incorrectKills: 0,
+      correctProsecutes: 0,
+      incorrectProsecutes: 0,
+      correctGuesses: 0,
+      incorrectGuesses: 0,
+      correctDeputyShoots: 0,
+      incorrectDeputyShoots: 0,
+      correctJailorExecutes: 0,
+      incorrectJailorExecutes: 0,
+      correctMedicShields: 0,
+      incorrectMedicShields: 0,
+      correctWardenFortifies: 0,
+      incorrectWardenFortifies: 0,
+      janitorCleans: 0,
+      completedTasks: 2,
+      survivedRounds: 3,
+      correctAltruistRevives: 0,
+      incorrectAltruistRevives: 0,
+      correctSwaps: 0,
+      incorrectSwaps: 0,
+      totalPoints: 2.8,
+    },
+    {
+      playerName: "Budyn",
+      roleHistory: ["Juggernaut"],
+      modifiers: [],
+      win: 0,
+      disconnected: 0,
+      initialRolePoints: 3,
+      correctKills: 0,
+      incorrectKills: 0,
+      correctProsecutes: 0,
+      incorrectProsecutes: 0,
+      correctGuesses: 0,
+      incorrectGuesses: 0,
+      correctDeputyShoots: 0,
+      incorrectDeputyShoots: 0,
+      correctJailorExecutes: 0,
+      incorrectJailorExecutes: 0,
+      correctMedicShields: 0,
+      incorrectMedicShields: 0,
+      correctWardenFortifies: 0,
+      incorrectWardenFortifies: 0,
+      janitorCleans: 0,
+      completedTasks: 0,
+      survivedRounds: 3,
+      correctAltruistRevives: 0,
+      incorrectAltruistRevives: 0,
+      correctSwaps: 0,
+      incorrectSwaps: 0,
+      totalPoints: 3.6,
+    },
+    {
+      playerName: "Malkiz",
+      roleHistory: ["Grenadier"],
+      modifiers: ["Double Shot", "Tiebreaker"],
+      win: 1,
+      disconnected: 0,
+      initialRolePoints: 2,
+      correctKills: 2,
+      incorrectKills: 1,
+      correctProsecutes: 0,
+      incorrectProsecutes: 0,
+      correctGuesses: 0,
+      incorrectGuesses: 0,
+      correctDeputyShoots: 0,
+      incorrectDeputyShoots: 0,
+      correctJailorExecutes: 0,
+      incorrectJailorExecutes: 0,
+      correctMedicShields: 0,
+      incorrectMedicShields: 0,
+      correctWardenFortifies: 0,
+      incorrectWardenFortifies: 0,
+      janitorCleans: 0,
+      completedTasks: 0,
+      survivedRounds: 4,
+      correctAltruistRevives: 0,
+      incorrectAltruistRevives: 0,
+      correctSwaps: 0,
+      incorrectSwaps: 0,
+      totalPoints: 7.8,
+    },
+    {
+      playerName: "Dziekansqr",
+      roleHistory: ["Tracker"],
+      modifiers: ["Bait"],
+      win: 0,
+      disconnected: 0,
+      initialRolePoints: 2,
+      correctKills: 0,
+      incorrectKills: 0,
+      correctProsecutes: 0,
+      incorrectProsecutes: 0,
+      correctGuesses: 0,
+      incorrectGuesses: 0,
+      correctDeputyShoots: 0,
+      incorrectDeputyShoots: 0,
+      correctJailorExecutes: 0,
+      incorrectJailorExecutes: 0,
+      correctMedicShields: 0,
+      incorrectMedicShields: 0,
+      correctWardenFortifies: 0,
+      incorrectWardenFortifies: 0,
+      janitorCleans: 0,
+      completedTasks: 6,
+      survivedRounds: 0,
+      correctAltruistRevives: 0,
+      incorrectAltruistRevives: 0,
+      correctSwaps: 0,
+      incorrectSwaps: 0,
+      totalPoints: 2.6,
+    },
+    {
+      playerName: "Arbuzix",
+      roleHistory: ["Survivor"],
+      modifiers: ["Satellite"],
+      win: 0,
+      disconnected: 0,
+      initialRolePoints: 3,
+      correctKills: 0,
+      incorrectKills: 0,
+      correctProsecutes: 0,
+      incorrectProsecutes: 0,
+      correctGuesses: 0,
+      incorrectGuesses: 0,
+      correctDeputyShoots: 0,
+      incorrectDeputyShoots: 0,
+      correctJailorExecutes: 0,
+      incorrectJailorExecutes: 0,
+      correctMedicShields: 0,
+      incorrectMedicShields: 0,
+      correctWardenFortifies: 0,
+      incorrectWardenFortifies: 0,
+      janitorCleans: 0,
+      completedTasks: 0,
+      survivedRounds: 4,
+      correctAltruistRevives: 0,
+      incorrectAltruistRevives: 0,
+      correctSwaps: 0,
+      incorrectSwaps: 0,
+      totalPoints: 3.8,
+    },
+    {
+      playerName: "DawDu",
+      roleHistory: ["Prosecutor"],
+      modifiers: ["Multitasker"],
+      win: 0,
+      disconnected: 0,
+      initialRolePoints: 2,
+      correctKills: 0,
+      incorrectKills: 0,
+      correctProsecutes: 1,
+      incorrectProsecutes: 0,
+      correctGuesses: 0,
+      incorrectGuesses: 0,
+      correctDeputyShoots: 0,
+      incorrectDeputyShoots: 0,
+      correctJailorExecutes: 0,
+      incorrectJailorExecutes: 0,
+      correctMedicShields: 0,
+      incorrectMedicShields: 0,
+      correctWardenFortifies: 0,
+      incorrectWardenFortifies: 0,
+      janitorCleans: 0,
+      completedTasks: 4,
+      survivedRounds: 4,
+      correctAltruistRevives: 0,
+      incorrectAltruistRevives: 0,
+      correctSwaps: 0,
+      incorrectSwaps: 0,
+      totalPoints: 4.2,
+    },
+    {
+      playerName: "QukaVadi",
+      roleHistory: ["Imitator"],
+      modifiers: ["Taskmaster", "Immovable"],
+      win: 0,
+      disconnected: 0,
+      initialRolePoints: 2,
+      correctKills: 0,
+      incorrectKills: 0,
+      correctProsecutes: 0,
+      incorrectProsecutes: 0,
+      correctGuesses: 0,
+      incorrectGuesses: 0,
+      correctDeputyShoots: 0,
+      incorrectDeputyShoots: 0,
+      correctJailorExecutes: 0,
+      incorrectJailorExecutes: 0,
+      correctMedicShields: 0,
+      incorrectMedicShields: 0,
+      correctWardenFortifies: 0,
+      incorrectWardenFortifies: 0,
+      janitorCleans: 0,
+      completedTasks: 5,
+      survivedRounds: 0,
+      correctAltruistRevives: 0,
+      incorrectAltruistRevives: 0,
+      correctSwaps: 0,
+      incorrectSwaps: 0,
+      totalPoints: 2.5,
+    },
+    {
+      playerName: "Nudna",
+      roleHistory: ["Arsonist"],
+      modifiers: [],
+      win: 0,
+      disconnected: 0,
+      initialRolePoints: 3,
+      correctKills: 0,
+      incorrectKills: 0,
+      correctProsecutes: 0,
+      incorrectProsecutes: 0,
+      correctGuesses: 0,
+      incorrectGuesses: 0,
+      correctDeputyShoots: 0,
+      incorrectDeputyShoots: 0,
+      correctJailorExecutes: 0,
+      incorrectJailorExecutes: 0,
+      correctMedicShields: 0,
+      incorrectMedicShields: 0,
+      correctWardenFortifies: 0,
+      incorrectWardenFortifies: 0,
+      janitorCleans: 0,
+      completedTasks: 0,
+      survivedRounds: 3,
+      correctAltruistRevives: 0,
+      incorrectAltruistRevives: 0,
+      correctSwaps: 0,
+      incorrectSwaps: 0,
+      totalPoints: 3.6,
+    },
+    {
+      playerName: "Miras",
+      roleHistory: ["Vigilante"],
+      modifiers: ["Celebrity", "Sixth Sense"],
+      win: 0,
+      disconnected: 0,
+      initialRolePoints: 2,
+      correctKills: 0,
+      incorrectKills: 0,
+      correctProsecutes: 0,
+      incorrectProsecutes: 0,
+      correctGuesses: 0,
+      incorrectGuesses: 1,
+      correctDeputyShoots: 0,
+      incorrectDeputyShoots: 0,
+      correctJailorExecutes: 0,
+      incorrectJailorExecutes: 0,
+      correctMedicShields: 0,
+      incorrectMedicShields: 0,
+      correctWardenFortifies: 0,
+      incorrectWardenFortifies: 0,
+      janitorCleans: 0,
+      completedTasks: 4,
+      survivedRounds: 3,
+      correctAltruistRevives: 0,
+      incorrectAltruistRevives: 0,
+      correctSwaps: 0,
+      incorrectSwaps: 0,
+      totalPoints: 1.0,
+    }
+  ],
+  gameEvents: [
+    {
+      timestamp: "18:54:55",
+      description: "Malkiz (Grenadier) killed QukaVadi (Imitator) — Correct kill",
+    },
+    {
+      timestamp: "18:54:58",
+      description: "Cleopatrie (Janitor) killed Dziekansqr (Tracker) — Correct kill",
+    },
+    {
+      timestamp: "18:56:38",
+      description: "DawDu (Prosecutor) prosecuted Cleopatrie (Janitor) — Correct prosecute",
+    },
+    {
+      timestamp: "18:57:41",
+      description: "brubel (Warlock) guessed Mamika (Executioner) — Correct guess",
+    },
+    {
+      timestamp: "19:00:21",
+      description: "Miras (Vigilante) guessed Miras (Vigilante) — Incorrect guess",
+    },
+    {
+      timestamp: "19:00:30",
+      description: "brubel (Warlock) guessed Bushej (Swapper) — Correct guess",
+    },
+    {
+      timestamp: "19:01:05",
+      description: "brubel (Warlock) guessed Nudna (Arsonist) — Correct guess",
+    },
+    {
+      timestamp: "19:02:03",
+      description: "brubel (Warlock) killed Budyn (Juggernaut) — Correct kill",
+    },
+    {
+      timestamp: "19:02:04",
+      description: "Malkiz (Grenadier) killed brubel (Warlock) — Incorrect kill",
+    },
+    {
+      timestamp: "19:02:24",
+      description: "Malkiz (Grenadier) killed Dusia (Lookout) — Correct kill",
+    }
+  ],
+  meetings: [
+    {
+      meetingNumber: 1,
+      deathsSinceLastMeeting: [
+        "QukaVadi (Imitator) - Killed by Malkiz (Grenadier)"
+,        "Dziekansqr (Tracker) - Killed by Cleopatrie (Janitor)"
+      ],
+      votes: {
+        "Cleopatrie": ["Arbuzix", "Dusia", "ziomson", "Nudna", "DawDu", "Budyn", "Mamika", "Bushej", "Malkiz", "Miras"],
+        "ziomson": ["MikoTheOwl"]
+      },
+      skipVotes: [
+"Cleopatrie", "brubel"
+      ],
+      noVotes: [
+
+      ],
+      blackmailedPlayers: [
+
+      ],
+      jailedPlayers: [
+
+      ],
+      wasTie: false,
+      wasBlessed: false,
+    }
+,
+    {
+      meetingNumber: 2,
+      deathsSinceLastMeeting: [
+        "Cleopatrie (Janitor) - Voted out in Meeting 1"
+      ],
+      votes: {
+        "ziomson": ["MikoTheOwl", "DawDu", "Malkiz", "brubel", "Arbuzix", "Bushej"]
+      },
+      skipVotes: [
+"ziomson", "Dusia", "Budyn", "Nudna"
+      ],
+      noVotes: [
+
+      ],
+      blackmailedPlayers: [
+
+      ],
+      jailedPlayers: [
+
+      ],
+      wasTie: false,
+      wasBlessed: false,
+    }
+,
+    {
+      meetingNumber: 3,
+      deathsSinceLastMeeting: [
+        "ziomson (Trapper) - Voted out in Meeting 2"
+      ],
+      votes: {
+        "Bushej": ["MikoTheOwl"],
+        "MikoTheOwl": ["MikoTheOwl", "Dusia", "Arbuzix", "brubel"],
+        "DawDu": ["Budyn"]
+      },
+      skipVotes: [
+"DawDu", "Malkiz"
+      ],
+      noVotes: [
+
+      ],
+      blackmailedPlayers: [
+
+      ],
+      jailedPlayers: [
+
+      ],
+      wasTie: false,
+      wasBlessed: false,
+    }
+,
+    {
+      meetingNumber: 4,
+      deathsSinceLastMeeting: [
+        "MikoTheOwl (Hunter) - Voted out in Meeting 3"
+,        "Budyn (Juggernaut) - Killed by brubel (Warlock)"
+,        "brubel (Warlock) - Killed by Malkiz (Grenadier)"
+,        "Dusia (Lookout) - Killed by Malkiz (Grenadier)"
+      ],
+      votes: {
+        "Arbuzix": ["Malkiz", "DawDu"],
+        "DawDu": ["Arbuzix"]
+      },
+      skipVotes: [
+
+      ],
+      noVotes: [
+
+      ],
+      blackmailedPlayers: [
+
+      ],
+      jailedPlayers: [
+
+      ],
+      wasTie: false,
+      wasBlessed: false,
+    }
+  ],
+};
