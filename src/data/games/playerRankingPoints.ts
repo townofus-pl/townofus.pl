a22e5a51a724ebc5f8ec8966271ddb3b313dc401
MalkizH3 <114110029+MalkizH3@users.noreply.github.com>
2025-08-21 13:53:45 +0200

    Update playerRankingPoints.ts

--- src/data/games/playerRankingPoints.ts
+++ src/data/games/playerRankingPoints.ts
@@ -4,19 +4,18 @@
 // "nick": 2100,
 
 const playerRankingPoints: Record<string, number> = {
-    "ziomson": 2339,
-    "Cleopatrie": 2147,
-    "Dziekansqr": 2101,
-    "brubel": 2070,
-    "Zieloony": 2038,
-    "Mamika": 2036,
-    "Budyn": 2010,
-    "QukaVadi": 2009,
-    "Miras": 1982,
-    "DawDu": 1968,
-    "Malkiz": 1931,
-    "Bushej": 1900,
-    "MikoTheOwl": 1894,
+    "Cleopatrie": 2303,
+    "ziomson": 2224,
+    "brubel": 2165,
+    "Dziekansqr": 2133,
+    "DawDu": 2122,
+    "Mamika": 2077,
+    "Jakubeq": 2016,
+    "Nudna": 1983,
+    "QukaVadi": 1969,
+    "Zieloony": 1961,
+    "Miras": 1926,
+    "Budyn": 1907,
 };
 
 export default playerRankingPoints;
